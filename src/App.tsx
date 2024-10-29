5ad88475c204ad8dcfbd377e100e55f07de1d823
Igor Mokhov <igormokhovid@gmail.com>
2024-10-29 10:36:59 +0300

    feat: added pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { SignInPage } from './pages/SignInPage';
+import { SignUpPage } from './pages/SignUpPage';
+import { ResetPasswordPage } from './pages/ResetPasswordPage';
+
 export const App = () => {
-  return <h1>Mee Team App</h1>;
+  return (
+    <>
+      <Routes>
+        <Route path="signin" element={<SignInPage />} />
+        <Route path="signup" element={<SignUpPage />} />
+        <Route path="reset-password" element={<ResetPasswordPage />} />
+
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
+  );
 };

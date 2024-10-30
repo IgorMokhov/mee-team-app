17c786f8b4cc27f3a3ce22ac08a899489c0ade46
Igor Mokhov <igormokhovid@gmail.com>
2024-10-30 11:53:02 +0300

    feat: added signin request in the SignIn

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,7 +1,12 @@
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Logo } from '../../UI/Logo/Logo';
 import arrowIcon from '../../assets/icons/arrowIcon.svg';
+import { useAppDispatch } from '../../redux/hooks';
+import { useSigninMutation } from '../../redux/userApi';
+import { ErrorResponse } from '../../types/error';
+import { useEffect } from 'react';
+import { setToken } from '../../redux/slices/auth/authSlice';
 import styles from './SignIn.module.scss';
 
 interface SignInForm {
@@ -10,11 +15,31 @@ interface SignInForm {
 }
 
 export const SignIn = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
   const { register, handleSubmit } = useForm<SignInForm>();
 
-  const onSubmit: SubmitHandler<SignInForm> = (data) => {
-    console.log(data);
+  const [signin, { isSuccess, data: dataSignin, error: errorSignin }] =
+    useSigninMutation();
+
+  const onSubmit: SubmitHandler<SignInForm> = async ({ email, password }) => {
+    await signin({
+      email,
+      password,
+      lang: 'en',
+    }).unwrap();
   };
+
+  console.log((errorSignin as ErrorResponse)?.data.message); // we can show this error in the app
+
+  useEffect(() => {
+    if (isSuccess && dataSignin) {
+      const token = dataSignin.data.token;
+      dispatch(setToken(token));
+      navigate('/');
+    }
+  }, [isSuccess, dataSignin]);
 
   return (
     <div className={styles.wrapper}>
@@ -35,6 +60,7 @@ export const SignIn = () => {
           <input
             {...register('password', { required: true })}
             type="password"
+            placeholder="********************"
             id="password"
           />
           <Link className={styles.signin_text} to="/reset-password">

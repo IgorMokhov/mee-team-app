003eca8890c7586e52bf302521b95e138c8ddace
Igor Mokhov <igormokhovid@gmail.com>
2024-10-31 12:33:27 +0300

    feat: added util extractLocalEmployee

---
+++ src/utils/extractLocalEmployee.ts
@@ -0,0 +1,12 @@
+import { LocalEmployee, ServerEmployee } from '../types/employees';
+
+export const extractLocalEmployee = (
+  employee: ServerEmployee
+): LocalEmployee => ({
+  employeeId: employee.profile.employee_id,
+  firstName: employee.profile.first_name,
+  lastName: employee.profile.last_name,
+  email: employee.profile.personal_email,
+  phone: employee.profile.mobile_phone,
+  statusDismiss: employee.status.dismiss,
+});

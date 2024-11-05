d53f1cb6bec9ff8e5fa7f528ef982d6a0a6ead82
Igor Mokhov <igormokhovid@gmail.com>
2024-11-05 18:14:47 +0300

    feat: added state in the EmployeeList. reafactored

--- src/components/EmployeeList/EmployeeList.tsx
+++ src/components/EmployeeList/EmployeeList.tsx
@@ -1,19 +1,35 @@
+import { useState } from 'react';
 import { PORTAL_ID } from '../../config/constants';
 import { useAppSelector } from '../../redux/hooks';
-import { useGetAllEmployeesQuery } from '../../redux/userApi';
-import { ServerEmployee } from '../../types/employees';
+import {
+  useEditEmployeeMutation,
+  useGetAllEmployeesQuery,
+} from '../../redux/userApi';
+import { LocalEmployee, ServerEmployee } from '../../types/employees';
 import { ErrorResponse } from '../../types/error';
 import { extractLocalEmployee } from '../../utils/extractLocalEmployee';
 import { Employee } from '../Employee/Employee';
+import { Modal } from '../../UI/Modal/Modal';
 import styles from './EmployeeList.module.scss';
 
 export const EmployeeList = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedEmployee, setSelectedEmployee] =
+    useState<null | LocalEmployee>(null);
+
+  const [editEmployee, { data: dataEdit, error: errorEdit }] =
+    useEditEmployeeMutation();
+
+  console.log('data:', dataEdit);
+  console.log('error:', errorEdit);
+
   const token = useAppSelector((state) => state.auth.token);
 
   const {
     data: employees,
-    error,
-    isLoading,
+    error: errorEmployees,
+    isLoading: isLoadingEmployees,
+    refetch,
   } = useGetAllEmployeesQuery(PORTAL_ID, {
     skip: !token,
   });
@@ -21,23 +37,36 @@ export const EmployeeList = () => {
   const fireEmployee = (id: number) => {};
   const removeEmployee = (id: number) => {};
 
-  if (isLoading) {
+  if (isLoadingEmployees) {
     return <h3>Loading...</h3>;
   }
-  if (error) {
-    return <h3>Error: {(error as ErrorResponse).data.message}</h3>;
+  if (errorEmployees) {
+    return <h3>Error: {(errorEmployees as ErrorResponse).data.message}</h3>;
   }
 
   return (
-    <ul className={styles.emplist}>
-      {employees?.data.map((employee: ServerEmployee) => (
-        <Employee
-          key={employee.profile.employee_id}
-          {...extractLocalEmployee(employee)}
-          fireEmployee={fireEmployee}
-          removeEmployee={removeEmployee}
+    <>
+      <ul className={styles.emplist}>
+        {Array.isArray(employees?.data) &&
+          employees?.data.map((employee: ServerEmployee) => (
+            <Employee
+              key={employee.profile.employee_id}
+              {...extractLocalEmployee(employee)}
+              fireEmployee={fireEmployee}
+              removeEmployee={removeEmployee}
+              openModal={() => setIsModalOpen(true)}
+              setSelectedEmployee={setSelectedEmployee}
+            />
+          ))}
+      </ul>
+      {isModalOpen && (
+        <Modal
+          onClose={() => setIsModalOpen(false)}
+          selectedEmployee={selectedEmployee!}
+          editEmployee={editEmployee}
+          refetchEmployees={refetch}
         />
-      ))}
-    </ul>
+      )}
+    </>
   );
 };

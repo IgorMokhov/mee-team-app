bf94ecbe58efa8844d0ff7b63eef5cc5809ec49c
Igor Mokhov <igormokhovid@gmail.com>
2024-11-06 15:25:30 +0300

    feat: added navigate in the Dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,11 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import { EmployeeList } from '../EmployeeList/EmployeeList';
 import styles from './Dashboard.module.scss';
 
 export const Dashboard = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.dashboard}>
       <h2 className={styles.dashboard_title}>Configuration</h2>
-      <button className={styles.dashboard_add_emp}>
+      <button
+        className={styles.dashboard_add_emp}
+        onClick={() => navigate('add-new-employee')}
+      >
         Ajouter un utilisateur
       </button>
 
